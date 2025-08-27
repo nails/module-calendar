ee3a220d65e0315deecc99865b295e0a53de2e48
Pablo de la Peña <p@shedcollective.org>
2025-08-27 13:26:25 +0100

    feat: Add Constants class

---
+++ src/Constants.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Nails\Calendar;
+
+/**
+ * Class Constants
+ *
+ * @package Nails\Calendar
+ */
+class Constants
+{
+    /**
+     * The slug for this module
+     *
+     * @var string
+     */
+    const MODULE_SLUG = 'nails/module-calendar';
+}
